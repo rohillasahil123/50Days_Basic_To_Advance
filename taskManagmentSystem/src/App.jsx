51281c1653073c6248fb92bbcd464ffcbf89e097
Sahil Rohilla <rohillasahil704@gmail.com>
2026-01-21 00:17:36 +0530

    task

---
+++ taskManagmentSystem/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-blue-800  font-bold text-4xl'>
+      <h1>Task managment</h1>
+      
+    </div>
+  )
+}
+
+export default App
